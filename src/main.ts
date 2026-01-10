ed8ceebf3cc24f4e9b517e3d8622dbbb844b6583
Santos Palma <tiagosantos.mr@gmail.com>
2026-01-10 15:03:55 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { invoke } from "@tauri-apps/api/core";
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+const button = document.getElementById("next")!;
+
+button.addEventListener("click", () => {
+  alert("Resposta registrada!");
+});
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
